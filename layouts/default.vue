4bb8a0722404138d01249eb15daad8af8714554b
NickCrawford <hi@nickcrawford.design>
2020-02-17 15:50:14 -0500

    Adds footer bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,14 @@
     <transition name="layout" mode="out-in">
       <div>
         <nuxt />
+
+        <div class="page-footer">
+          <div class="footer-content">
+            <div>
+              <p>hi@nickcrawford.design</p>
+            </div>
+          </div>
+        </div>
       </div>
     </transition>
 
@@ -17,4 +25,24 @@
 
 <style lang="scss">
 @import '~assets/styles/variables';
+
+.page-footer {
+  // background: white;
+  color: $success;
+}
+
+.footer-content {
+  padding: 2rem;
+
+  display: flex;
+  justify-content: center;
+
+  p {
+    margin: 0;
+  }
+
+  .is-white {
+    color: white;
+  }
+}
 </style>
